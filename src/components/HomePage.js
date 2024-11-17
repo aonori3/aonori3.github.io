91d0c949e6ade203b759026e3f016fca9ee96c83
aoiotani <aoiohtani@gmail.com>
2024-11-17 18:31:54 -0500

    updated interests

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,17 +9,20 @@ const HomePage = () => {
       {/* <img src="/pfp.jpg" alt="Aoi Otani" className="profile-pic" />*/}
       <div className="bio">
         <p className="fira-sans-regular">
-          I am currently a senior (fourth-year) at Harvard, studying Biology and Computer Science. 
+        I am a senior (fourth-year) at Harvard, studying Biology and Computer Science. 
+        My mission is to enhance human quality of life through technology. 
+
         </p>
         <p className="fira-sans-regular">
           My interests include, but are not limited to:
         </p>
         <ul className="list">
-          <li className="fira-sans-regular">Mitigating catastrophic forgetting in machine learning models</li>
-          <li className="fira-sans-regular">The intersection between artificial and natural intelligence</li>
-          <li className="fira-sans-regular">Improving the quality of text-to-image models</li>
-          <li className="fira-sans-regular">Software tools that accelerate biological discovery</li>
-          <li className="fira-sans-regular">Software tools to improve healthcare</li>
+          <li className="fira-sans-regular">Tackling Catastrophic Forgetting in AI models</li>
+          <li className="fira-sans-regular">LLM Safety, Adversarial Attacks</li>
+          <li className="fira-sans-regular">Improving the quality of text-image generative models</li>
+          <li className="fira-sans-regular">Tools and models that accelerate biological discovery</li>
+          <li className="fira-sans-regular">Software tools to improve healthcare system</li>
+          <li className="fira-sans-regular">How the human brain works</li>
         </ul>
       </div>
 
