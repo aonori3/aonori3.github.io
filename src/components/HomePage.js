97756105670a62d8211aad3c11c2ca5221a2e5ad
aoiotani <aoiohtani@gmail.com>
2024-12-17 19:22:58 -0500

    changed descriptions

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,7 +10,8 @@ const HomePage = () => {
       <div className="bio">
         <p className="fira-sans-regular">
         I am a senior (fourth-year) at Harvard, studying Biology and Computer Science. 
-        My mission is to enhance human quality of life through technology. 
+        My mission is to enhance human quality of life through technology through startup(s) 
+        and research.
 
         </p>
         <p className="fira-sans-regular">
@@ -18,24 +19,26 @@ const HomePage = () => {
         </p>
         <ul className="list">
           <li className="fira-sans-regular">Tackling Catastrophic Forgetting in AI models</li>
-          <li className="fira-sans-regular">LLM Safety, Adversarial Attacks</li>
-          <li className="fira-sans-regular">Improving the quality of text-image generative models</li>
-          <li className="fira-sans-regular">Tools and models that accelerate biological discovery</li>
-          <li className="fira-sans-regular">Software tools to improve healthcare system</li>
+          <li className="fira-sans-regular">LLM safety - improving robustness against adversarial attacks</li>
+          <li className="fira-sans-regular">Tools and models that accelerate biological discovery (e.g. transformer models for molecular dynamics simulations) </li>
+          <li className="fira-sans-regular">Software tools to improve healthcare. Applications in hospitals, clinical trials, telehealth.</li>
           <li className="fira-sans-regular">How the human brain works</li>
+          <li className="fira-sans-regular">Emotionally intelligent AI and AI companions </li>
+          <li className="fira-sans-regular">Integrating biometric data and wearables for health monitoring and analysis</li>
         </ul>
       </div>
-
+{/* 
       <div className="projects-section">
         <h2 className="fira-sans-medium">Projects</h2>
         <ul className="list">
           <li><Link to="/projects" className="fira-sans-regular">Symbia</Link></li>
+          <li><Link to="/projects" className="fira-sans-regular">PolicyMART</Link></li>
           <li><Link to="/projects" className="fira-sans-regular">Mitigating Catastrophic Forgetting in Continual Learning</Link></li>
           <li><Link to="/projects" className="fira-sans-regular">LungTech</Link></li>
           <li><Link to="/projects" className="fira-sans-regular">Recipe Generator App</Link></li>
           <li><Link to="/projects" className="fira-sans-regular">Splice Predictor</Link></li>
         </ul>
-      </div>
+      </div> */}
       
     </div>
   );
